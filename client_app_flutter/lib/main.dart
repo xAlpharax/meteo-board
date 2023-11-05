c2b4fccc4dd0a7087e7d0dfd2d6cf9ca31bbb775
xAlpharax <42233094+xAlpharax@users.noreply.github.com>
2023-11-05 04:46:48 +0200

    Hooking up the whole firebase of the project via firebase CLI.
    Also used flutter_fire firebase_core among several other libs.
    
    Changes to be committed:
        new file:   client_app_flutter/android/app/google-services.json
        modified:   client_app_flutter/android/build.gradle
        new file:   client_app_flutter/ios/Runner/GoogleService-Info.plist
        new file:   client_app_flutter/ios/firebase_app_id_file.json
        new file:   client_app_flutter/lib/firebase_options.dart
        modified:   client_app_flutter/lib/main.dart
        modified:   client_app_flutter/pubspec.lock
        modified:   client_app_flutter/pubspec.yaml

--- client_app_flutter/lib/main.dart
+++ client_app_flutter/lib/main.dart
@@ -1,6 +1,11 @@
 import 'package:flutter/material.dart';
 
-void main() {
+import 'package:firebase_core/firebase_core.dart';
+import 'package:client_app_flutter/firebase_options.dart';
+
+Future <void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MyApp());
 }
 
