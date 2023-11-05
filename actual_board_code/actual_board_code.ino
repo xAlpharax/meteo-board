3fc1f858657dfca45ff0ec3e1ea4ff975928a13d
Alphara <42233094+xAlpharax@users.noreply.github.com>
2023-11-05 10:35:14 +0000

    Made changes in the secondary loop so that DSM501A can calculate its things.
    Changes to be committed:
    	modified:   actual_board_code/actual_board_code.ino

--- actual_board_code/actual_board_code.ino
+++ actual_board_code/actual_board_code.ino
@@ -39,7 +39,7 @@ unsigned long sampletime_ms = 1000;
 unsigned long lowpulseoccupancy = 0;
 float ratio = 0;
 float concentration = 0;
-uint32_t delayMS = 1000; // in loop delay() explicited
+uint32_t delayMS = 2000; // in loop delay() explicited
 
 // WiFI stuff
 
@@ -116,18 +116,22 @@ void loop() { // Main code
 
   duration = pulseIn(DSM501, LOW);
   lowpulseoccupancy += duration;
-  endtime = millis();
-
-  ratio = (lowpulseoccupancy-endtime+starttime + sampletime_ms)/(sampletime_ms*10.0);
-  concentration = 1.1*pow(ratio,3)-3.8*pow(ratio,2)+520*ratio+0.62; // idk what s going on in here anymore.. it used to work, it doesn t now..
-  // concentration = 0.001915 * pow(low_ratio, 2) + 0.09522 * low_ratio - 0.04884;
-  Serial.print("lowpulseoccupancy: ");
-  Serial.print(lowpulseoccupancy);
-  Serial.print("    ratio: ");
-  Serial.print(ratio);
-  Serial.print("    DSM501A: ");
-  Serial.println(concentration);
-
-  lowpulseoccupancy = 0;
   starttime = millis();
+  endtime = starttime + sampletime_ms;
+
+  while ( starttime < endtime) {
+    ratio = (lowpulseoccupancy-endtime+starttime + sampletime_ms)/(sampletime_ms*10.0);
+    concentration = 1.1*pow(ratio,3)-3.8*pow(ratio,2)+520*ratio+0.62; // idk what s going on in here anymore.. it used to work, it doesn t now..
+    // concentration = 0.001915 * pow(low_ratio, 2) + 0.09522 * low_ratio - 0.04884;
+    Serial.print("lowpulseoccupancy: ");
+    Serial.print(lowpulseoccupancy);
+    Serial.print("    ratio: ");
+    Serial.print(ratio);
+    Serial.print("    DSM501A: ");
+    Serial.println(concentration);
+
+    lowpulseoccupancy = 0;
+    starttime = millis();
+    delay(2000);
+  }
 }
